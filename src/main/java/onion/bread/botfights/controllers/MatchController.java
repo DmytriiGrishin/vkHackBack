2b4ab5d285222ff970768974c10c8c2373d5daa5
dmitriy <dmytriigrishin@gmail.com>
2018-11-10 20:05:51 +0300

    Match status filtering

--- src/main/java/onion/bread/botfights/controllers/MatchController.java
+++ src/main/java/onion/bread/botfights/controllers/MatchController.java
@@ -3,6 +3,7 @@ package onion.bread.botfights.controllers;
 import lombok.RequiredArgsConstructor;
 import onion.bread.botfights.dao.MatchDao;
 import onion.bread.botfights.model.Match;
+import onion.bread.botfights.model.MatchStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,35 +16,48 @@ public class MatchController {
     private final MatchDao matchDao;
 
     @GetMapping("/matches/completed")
-    public Iterable<Match> find(@RequestParam(required = false) String teamId,
+    public Iterable<Match> findCompleted(@RequestParam(required = false) String teamId,
                             @RequestParam(required = false) String tournamentID,
                             @RequestParam(required = false) String timestamp) {
+        MatchStatus status = MatchStatus.COMPLETED;
+        return getMatches(teamId, tournamentID, timestamp, status);
+    }
+
+    @GetMapping("/matches/upcoming")
+    public Iterable<Match> findUpcoming(@RequestParam(required = false) String teamId,
+                                @RequestParam(required = false) String tournamentID,
+                                @RequestParam(required = false) String timestamp) {
+        MatchStatus status = MatchStatus.UPCOMING;
+        return getMatches(teamId, tournamentID, timestamp, status);
+    }
+
+    private Iterable<Match> getMatches(String teamId, String tournamentID, String timestamp, MatchStatus status) {
         if (teamId == null) {
             if (tournamentID == null) {
                 if (timestamp == null) {
-                    return matchDao.findAll();
+                    return matchDao.findAllByStatus(status);
                 } else {
-                    return matchDao.findAllByTimestamp(timestamp);
+                    return matchDao.findAllByTimestamp(timestamp, status);
                 }
             } else {
                 if (timestamp == null) {
-                    return matchDao.findAllByTournament(tournamentID);
+                    return matchDao.findAllByTournament(tournamentID, status);
                 } else {
-                    return matchDao.findAllByTimestampAndTournament(timestamp, tournamentID);
+                    return matchDao.findAllByTimestampAndTournament(timestamp, tournamentID, status);
                 }
             }
         } else {
             if (tournamentID == null) {
                 if (timestamp == null) {
-                    return matchDao.findAllByTeam(teamId);
+                    return matchDao.findAllByTeam(teamId, status);
                 } else {
-                    return matchDao.findAllByTimestampAndTeam(timestamp, teamId);
+                    return matchDao.findAllByTimestampAndTeam(timestamp, teamId, status);
                 }
             } else {
                 if (timestamp == null) {
-                    return matchDao.findAllByTournamentAndTeam(tournamentID, teamId);
+                    return matchDao.findAllByTournamentAndTeam(tournamentID, teamId, status);
                 } else {
-                    return matchDao.findAllByTimestampAndTournamentAndTeam(timestamp, tournamentID, teamId);
+                    return matchDao.findAllByTimestampAndTournamentAndTeam(timestamp, tournamentID, teamId, status);
                 }
             }
         }
--- src/main/java/onion/bread/botfights/dao/MatchDao.java
+++ src/main/java/onion/bread/botfights/dao/MatchDao.java
@@ -2,35 +2,44 @@ package onion.bread.botfights.dao;
 
 import onion.bread.botfights.model.Match;
 
+import onion.bread.botfights.model.MatchStatus;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface MatchDao extends CrudRepository<Match, String> {
     @Query("Select m from Match m " +
-            "where team1 = ?1 or team2 = ?1 or team3 = ?1 or team4 = ?1")
-    List<Match> findAllByTeam(String command);
+            "where team1 = ?1 or team2 = ?1 or team3 = ?1 or team4 = ?1 and status = ?2")
+    Iterable<Match> findAllByTeam(String command, MatchStatus status);
 
-    List<Match> findAllByTournament(String tournament);
-    List<Match> findAllByTimestamp(String timestamp);
+    @Query("Select m from Match m " +
+            "where tournament = ?1 and status = ?2")
+    Iterable<Match> findAllByTournament(String tournament, MatchStatus status);
+    @Query("Select m from Match m " +
+            "where timestamp = ?1 and status = ?2")
+    Iterable<Match> findAllByTimestamp(String timestamp, MatchStatus status);
 
     @Query("Select m from Match m " +
             "where timestamp = ?1 and " +
-            "(team1 = ?2 or team2 = ?2 or team3 = ?2 or team4 = ?2)")
-    List<Match> findAllByTimestampAndTeam(String timestamp, String command1);
-
-    List<Match> findAllByTimestampAndTournament(String timestamp, String tournament);
+            "(team1 = ?2 or team2 = ?2 or team3 = ?2 or team4 = ?2) and status = ?3 ")
+    Iterable<Match> findAllByTimestampAndTeam(String timestamp, String command1, MatchStatus status);
+    @Query("Select m from Match m " +
+            "where timestamp = ?1 and tournament = ?2 and status = ?3")
+    Iterable<Match> findAllByTimestampAndTournament(String timestamp, String tournament, MatchStatus status);
 
     @Query("Select m from Match m " +
             "where timestamp = ?1 and tournament = ?3 and " +
-            "(team1 = ?2 or team2 = ?2 or team3 = ?2 or team4 = ?2)")
-    List<Match> findAllByTimestampAndTournamentAndTeam(String timestamp, String command, String tournament);
+            "(team1 = ?2 or team2 = ?2 or team3 = ?2 or team4 = ?2) and status = ?4")
+    Iterable<Match> findAllByTimestampAndTournamentAndTeam(String timestamp, String command, String tournament, MatchStatus status);
 
     @Query("Select m from Match m " +
-            "where tournament = ?0 and " +
-            "(team1 = ?1 or team2 = ?1 or team3 = ?1 or team4 = ?1)")
-    List<Match> findAllByTournamentAndTeam(String tournament, String teamId);
+            "where tournament = ?1 and " +
+            "(team1 = ?2 or team2 = ?2 or team3 = ?2 or team4 = ?2) and status = ?3")
+    Iterable<Match> findAllByTournamentAndTeam(String tournament, String teamId, MatchStatus status);
+
+    @Query("Select m from Match m " +
+            "where status = ?1")
+    Iterable<Match> findAllByStatus(MatchStatus status);
 }
